93dbc268f85fc169eec1512b8c0732f275fa5b67
Zoreslav Khimich <zoreslav.khimich@gmail.com>
2012-12-26 16:43:24 +0700

    uShare updated with new graphics, UX flow and is compatible with the new widget now

--- Examples/ushare/ushare/USViewController.h
+++ Examples/ushare/ushare/USViewController.h
@@ -10,8 +10,22 @@
 #import <UCWidget.h>
 
 @interface USViewController : UIViewController<UCWidgetDelegate, UINavigationControllerDelegate>
-- (IBAction)ushareTapped:(id)sender;
+
+- (IBAction)share:(id)sender;
+- (IBAction)upload:(id)sender;
+
+@property (nonatomic, strong) NSURL *publicURL;
+@property (nonatomic, strong) NSString *fileName;
+@property (strong, nonatomic) UCWidget *uploadWidget;
 @property (weak, nonatomic) IBOutlet UILabel *promptLabel;
-@property (assign, nonatomic) BOOL busy;
+@property (weak, nonatomic) IBOutlet UIImageView *thumbnailImageView;
+@property (weak, nonatomic) IBOutlet UIView *uploadingAnchor;
+@property (weak, nonatomic) IBOutlet UIView *restingAnchor;
+@property (weak, nonatomic) IBOutlet UIToolbar *toolbar;
+@property (weak, nonatomic) IBOutlet UIButton *shareButton;
+@property (weak, nonatomic) IBOutlet UIProgressView *progressBar;
+@property (weak, nonatomic) IBOutlet UILabel *progressLabel;
+@property (weak, nonatomic) IBOutlet UILabel *uploadButtonHint;
+@property (weak, nonatomic) IBOutlet UILabel *uploadButtonHintArrow;
 
 @end
--- Examples/ushare/ushare/USViewController.m
+++ Examples/ushare/ushare/USViewController.m
@@ -7,10 +7,12 @@
 //
 
 #import "USViewController.h"
-
+#import "UIView+USHelpers.h"
 #import "UploadcareWidget.h"
 #import "AFNetworking.h"
 #import "AFJSONRequestOperation.h"
+#import "ARChromeActivity.h"
+#import "TUSafariActivity.h"
 
 #import <QuartzCore/QuartzCore.h>
 
@@ -27,13 +29,46 @@
     [super viewDidLoad];
     [self setJustStarted:YES];
     [[AFNetworkActivityIndicatorManager sharedManager] setEnabled:YES];
+        
+    /* thumbnail view */
+    self.thumbnailImageView.layer.cornerRadius = 4.0f;
+    self.thumbnailImageView.layer.masksToBounds = YES;
+    
+    /* share button */
+    [self.shareButton setBackgroundImage:[[UIImage imageNamed:@"USSilverButton"] resizableImageWithCapInsets:UIEdgeInsetsMake(16, 16, 16, 16)] forState:UIControlStateNormal];
+    [self.shareButton setBackgroundImage:[[UIImage imageNamed:@"USSilverButtonDepressed"] resizableImageWithCapInsets:UIEdgeInsetsMake(16, 16, 16, 16) resizingMode:UIImageResizingModeTile] forState:UIControlStateHighlighted];
+    
+    /* Uploadcare widget */
+    self.uploadWidget = [[UCWidget alloc]initWithUploadcarePublicKey:@"demopublickey"];
+    self.uploadWidget.delegate = self;
+    __weak USViewController *viewController = self;
+    self.uploadWidget.navigationBar.barStyle = UIBarStyleBlack;
+    
+    /* Social stuff */
+    [self.uploadWidget enableFacebook];
+    [self.uploadWidget enableFlickrWithAPIKey:@"2522a6f8bbff8fbb1826d335cad7d9b1" flickrAPISecret:@"4ab550f59749ca42"];
+    [self.uploadWidget enableInstagramWithClientId:@"e2a6987a814d4f5d96b24b6971f9eb89"];
+    
+    /* Handlers */
+    self.uploadWidget.uploadCompletionBlock = ^(NSString *fileId) {
+        [viewController onUploadedFileWithId:fileId];
+    };
+    self.uploadWidget.uploadProgressBlock = ^(long long bytesDone, long long bytesTotal) {
+        float progress = (float)bytesDone / bytesTotal;
+        [self.progressBar setProgress:progress animated:YES];
+        self.progressLabel.text = [NSString stringWithFormat:@"%@ %.0f%%", self.fileName, progress * 100.f];
+    };
+    self.uploadWidget.uploadFailureBlock = ^(NSError *error) {
+        [viewController onFailedToUploadBecauseOfError:error];
+    };
 }
 
 - (void)viewDidAppear:(BOOL)animated {
-    if (self.justStarted) {
-        self.justStarted = NO;
-        [self doTheBussiness];
-    }
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        [self.uploadButtonHint moveInFrom:kCATransitionFromBottom];
+        [self.uploadButtonHintArrow moveInFrom:kCATransitionFromBottom];
+    });
 }
 
 - (void)didReceiveMemoryWarning
@@ -42,10 +77,10 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)ushareTapped:(id)sender {
-    if (self.busy) return;
-    [self doTheBussiness];
-}
+//- (IBAction)ushareTapped:(id)sender {
+//    if (self.busy) return;
+//    [self presentUploadViewController];
+//}
 
 + (AFHTTPClient *)sharedHTTPClient {
     static AFHTTPClient *_sharedUploadClient = nil;
@@ -56,8 +91,24 @@
     return _sharedUploadClient;
 }
 
+- (void)addShadowAroundTheThumbnail {
+    /* Do this only once */
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSLog(@"ADDING SHADOW");
+        CALayer *shadowLayer = [CALayer layer];
+        [shadowLayer addSublayer:self.thumbnailImageView.layer];
+        shadowLayer.frame = self.view.bounds;
+        shadowLayer.shadowOffset = CGSizeMake(0, 2);
+        shadowLayer.shadowOpacity = .75f;
+        shadowLayer.shadowRadius = 3.75f;
+        [self.view.layer addSublayer:shadowLayer];
+    });
+}
 
-- (void)onUploadedFileWithId:(NSString *)fileId {
+
+- (void)onUploadedFileWithId:(NSString*)fileId {
+    self.promptLabel.text = NSLocalizedString(@"Almost there...", @"post-uploading pre-store text");
     /* Request our back-end to store the file and return the public URL */
     NSURLRequest *request = [[USViewController sharedHTTPClient] requestWithMethod:@"POST" path:@"http://ushare.whitescape.com/files/upload/" parameters:@{@"file_obj" : fileId}];
     AFJSONRequestOperation *op = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
@@ -69,61 +120,69 @@
 }
 
 
-- (void)blockUserInteraction:(BOOL)block {
-    if (block) {
-        self.view.layer.opacity = 0.3f;
-        self.busy = YES;
-    } else {
-        self.view.layer.opacity = 1.0f;
-        self.busy = NO;
-    }
-}
-
-- (void)uploadcareWidgetDidCancel:(UCWidget *)widget {
-    self.promptLabel.text = NSLocalizedString(@"uShare lets you upload files for free. Please keep the file sizes reasonable.", @"Uploadcare menu has been dismissed");
-    [self blockUserInteraction:NO];
-}
-
-- (void)doTheBussiness {
+- (void)presentUploadViewController {
     /* Setup the view controller */
-    UCWidget *uploadcare = [[UCWidget alloc]initWithUploadcarePublicKey:@"demopublickey"];
-    uploadcare.delegate = self;
-    [uploadcare setUploadCompletionBlock:^(NSString *fileId) {
-        [self blockUserInteraction:NO];
-        [self onUploadedFileWithId:fileId];
-    }];
-    
-    /* Social stuff */
-    [uploadcare enableFacebook];
-    [uploadcare enableFlickrWithAPIKey:@"2522a6f8bbff8fbb1826d335cad7d9b1" flickrAPISecret:@"4ab550f59749ca42"];
-    [uploadcare enableInstagramWithClientId:@"e2a6987a814d4f5d96b24b6971f9eb89"];
-    
-    /* Handlers */
-    [uploadcare setUploadFailureBlock:^(NSError *error) {
-        [self blockUserInteraction:NO];
-        [self onFailedToUploadBecauseOfError:error];
-    }];
-    [self presentViewController:uploadcare animated:YES completion:^{
-        self.promptLabel.text = NSLocalizedString(@"Just a moment please...", @"Uploading in progress text");
-        [self blockUserInteraction:YES];
+    [self presentViewController:self.uploadWidget animated:YES completion:^{
+        self.promptLabel.text = NSLocalizedString(@"", @"Uploading in progress text");
     }];
 }
 
-- (void)onUploadCompleteWithPublicAddress:(NSString *)publicAddress {
+- (void)uploadcareWidgetDidCancel:(UCWidget*)widget {
+//    self.promptLabel.text = NSLocalizedString(@"uShare lets you upload files for free. Please keep the file sizes reasonable.", @"Uploadcare menu has been dismissed");
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)uploadcareWidget:(UCWidget*)widget didStartUploadingFileNamed:(NSString*)fileName fromURL:(NSURL*)url withThumbnail:(UIImage*)thumbnail {
+    self.fileName = fileName;
+    [self dismissViewControllerAnimated:YES completion:^{
+        [self addShadowAroundTheThumbnail];
+        [UIView animateWithDuration:0.25f animations:^{
+            self.thumbnailImageView.image = thumbnail;
+            self.thumbnailImageView.frame = self.uploadingAnchor.frame;
+        }];
+        [self.progressBar setProgress:0 animated:NO];
+        self.progressLabel.text = fileName;
+        [self.progressBar moveInFrom:kCATransitionFromRight];
+        [self.progressLabel moveInFrom:kCATransitionFromRight];
+        [self.shareButton moveOutFrom:kCATransitionFromBottom];
+        [self.toolbar moveOutFrom:kCATransitionFromBottom];
+    }];
+}
+
+- (void)onUploadCompleteWithPublicAddress:(NSString*)publicAddress {
+    [UIView animateWithDuration:0.25f animations:^{
+        self.thumbnailImageView.frame = self.restingAnchor.frame;
+        [self.progressBar moveOutFrom:kCATransitionFromLeft];
+        [self.progressLabel moveOutFrom:kCATransitionFromLeft];
+    }];
+    self.publicURL = [NSURL URLWithString:publicAddress];
     self.promptLabel.text = NSLocalizedString(@"✅ The file has been successfully uploaded!", @"Upload success text");
-    
-    UIActivityViewController *actions = [[UIActivityViewController alloc]initWithActivityItems:@[publicAddress, [NSURL URLWithString:publicAddress]] applicationActivities:nil];
-    [self presentViewController:actions animated:YES completion:^{
-        
-    }];
+    [self.shareButton moveInFrom:kCATransitionFromTop];
+    [self.toolbar moveInFrom:kCATransitionFromTop];
 }
 
-- (void)onFailedToUploadBecauseOfError:(NSError *)error {
+- (void)onFailedToUploadBecauseOfError:(NSError*)error {
     self.promptLabel.text = NSLocalizedString(@"❌ Oops, something went wrong. Maybe try again?", @"Upload failure text");
+    [self.uploadWidget dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)viewDidUnload {
+    /* TODO: WTF ? */
     [self setPromptLabel:nil];
     [super viewDidUnload];
+}
+
+- (IBAction)share:(id)sender {
+        ARChromeActivity *openInChrome = [[ARChromeActivity alloc]init];
+        TUSafariActivity *openInSafari = [[TUSafariActivity alloc]init];
+        /* note: The URL *must* be the last object in the activity items array or else 'open in Chrome' activity won't work (...don't get me started) */
+        UIActivityViewController *actions = [[UIActivityViewController alloc]initWithActivityItems:@[self.publicURL.absoluteString, self.publicURL] applicationActivities:@[openInSafari, openInChrome]];
+        [self presentViewController:actions animated:YES completion:nil];
+}
+
+- (IBAction)upload:(id)sender {
+    self.uploadButtonHint.hidden = YES;
+    self.uploadButtonHintArrow.hidden = YES;
+    [self presentUploadViewController];
 }
 @end
