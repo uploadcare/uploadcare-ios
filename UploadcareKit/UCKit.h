58ccb5737de600198310b052d6b0101c3dcbdfac
Yury Nechaev <nechaev.main@gmail.com>
2016-04-05 16:43:32 +0300

    Main header file added

---
+++ UploadcareKit/UCKit.h
@@ -0,0 +1,14 @@
+//
+//  UCKit.h
+//  ExampleProject
+//
+//  Created by Yury Nechaev on 05.04.16.
+//  Copyright © 2016 Uploadcare. All rights reserved.
+//
+
+#import "UCClient.h"
+#import "UCFileUploadRequest.h"
+#import "UCRemoteFileUploadRequest.h"
+#import "UCFileInfoRequest.h"
+#import "UCGroupInfoRequest.h"
+#import "UCGroupPostRequest.h"
